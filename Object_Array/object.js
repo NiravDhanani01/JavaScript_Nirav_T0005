cb221a2394f27b11f2fdd6b2db7b98513c17cf05
NiravDhanani <nirav.devotee@gmail.com>
2024-06-28 13:42:11 +0530

    new task add

--- Object_Array/object.js
+++ Object_Array/object.js
@@ -63,3 +63,106 @@ Object.freeze(obj1)
 obj1.name = "raj"
 
 console.log('object1 after freezing =>',obj1);
+
+
+// combining of two obj 
+
+let names = {
+    name : "ABC",
+    age : 26,
+    id : 3,
+}
+let add = {
+    name : "xyz",
+    id : 3,
+}
+console.log({...names,...add});
+console.log(Object.assign("combine two obj",names,add))
+
+function swapKeysAndValues(obj) {
+    let newObj = {}
+
+    for (let i in obj) {
+        newObj[obj[i]] = i
+    }
+    return newObj
+}
+
+let swap = {
+    name: "ABC",
+    id: 6,
+}
+console.log(swapKeysAndValues(swap))
+
+// Write a function that takes an array of objects and a key, and returns an array of the values corresponding to that key in each object.
+function objs(arrval) {
+    let newarr = []
+   for(let i=0;i<arrval.length;i++){
+    newarr.push(arrval[i].name)
+    newarr.push(arrval[i].id)
+   }
+   return newarr
+}
+let arrval = [
+    { name: "ABC", id: 1 },
+    { name: "EFG", id: 2 },
+]
+console.log(objs(arrval));
+
+// sort obj by key 
+
+// Write a function that takes an array of objects and a key, and returns a new array of objects sorted by the values corresponding to that key.
+
+
+function sorting(objsort,key) {
+    return objsort.slice().sort((a,b)=>a[key] - b[key] )
+}
+
+
+function sorting(objsort, key) {
+
+    for (let i = 0; i < objsort.length; i++) {
+        for (let j = 0; j < objsort.length; j++) {
+            if (objsort[i][key] > objsort[j][key]) {
+                let temp = objsort[i];
+                objsort[i] = objsort[j];
+                objsort[j] = temp;
+            }
+        }
+    }
+    return objsort
+}
+
+let objsort = [
+    {
+        name: "ABC",
+        age: 16,
+    },
+    {
+        name: "EFG",
+        age: 12,
+    },
+    {
+        name: "XYZ",
+        age: 1,
+    },
+    {
+        name: "XYZ",
+        age: 16,
+    },
+    {
+        name: "XYZ",
+        age: 20,
+    }
+]
+console.log(sorting(objsort, "age"));
+
+
+
+
+
+
+
+
+
+
