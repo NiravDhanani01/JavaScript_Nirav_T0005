7d07c0c706e9a5209b6cf3e84548d6b989d1ab63
NiravDhanani <nirav.devotee@gmail.com>
2024-07-01 11:16:02 +0530

    continue..

---
+++ Array/Array.js
@@ -0,0 +1,27 @@
+let recipes = [
+    {
+        id : 1,
+        name : "lorem",
+        ingredients : "lorem,salt",
+        description : "lorerm inpsum nannd"
+    },
+    {
+        id : 2,
+        name : "lorem",
+        ingredients : "lorem,salt",
+        description : "lorerm inpsum nannd"
+    },
+]
+
+// here is add data
+let newRecip = {
+    id : 3,
+    name : "lorem",
+    ingredients : "lorem,salt",
+    description : "lorerm inpsum nannd"
+}
+recipes.push(newRecip)
+
+// delete 
+
+console.log(recipes);
