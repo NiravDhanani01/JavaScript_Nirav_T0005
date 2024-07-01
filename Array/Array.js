9288a288e952019f85e13711f61f91075079d0c6
NiravDhanani <nirav.devotee@gmail.com>
2024-07-01 15:42:36 +0530

    edit ..

--- Array/Array.js
+++ Array/Array.js
@@ -118,7 +118,7 @@ console.log("=============");
 
 // //edit
 // console.log("Edit data");
-// console.log(edit(1,"Dinner","Lorem ","hhel,ddd,ddf,fdf","hello"));
+// console.log(edit(1,"Dinner","Special Soup ","Boil water"," is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that"));
 // console.log("=========");
 
 // // filter data
